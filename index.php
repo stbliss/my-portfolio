5c15ca34fb151d8b7ee9a50da68b9477ca04d6e3
stbliss <stbliss75@gmail.com>
2021-07-13 09:32:23 -0700

    iportraite

--- index.php
+++ index.php
@@ -85,7 +85,7 @@
 
         <div class="row">
           <div class="col-lg-4" data-aos="fade-right">
-            <img src="../iPortfolio/photos/profile-portrait.jpg" class="img-fluid" alt="">
+            <img src="./photos/profile-portrait.jpg" class="img-fluid" alt="">
           </div>
           <div class="col-lg-8 pt-4 pt-lg-0 content" data-aos="fade-left">
             <h3>Digital Artiste, Web Developer &amp; Engineer</h3>
